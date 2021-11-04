279c8d16df78b53017a88e41d2f7151ce1685bca
Tejas M R <matblockgreninja@gmail.com>
2021-11-04 18:12:00 +0000

    Created compile.c and its binary

---
+++ compile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char **argv ) {
+
+    if( argc <= 1 ) {
+        printf("No file provided");
+        return 0;
+    }
+
+    char s[256] = "gcc ";
+    strcat( s, argv[1] );
+    strcat( s, ".c -o " );
+    strcat( s, argv[1] );
+    strcat( s, " -fopenmp" );
+    system( s );
+    
+}
